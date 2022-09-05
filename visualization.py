39cb4ce191a1feade3b2af53c56e6d5bb31c75e9
gondow86 <g.r.providence@icloud.com>
2022-09-06 02:26:28 +0900

    add python file for t-SNE

---
+++ visualization.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.manifold import TSNE
+
+digits = datasets.load_digits()
+
+print(digits.data.shape)
+# (1797, 64)
+
+print(digits.target.shape)
+# (1797,)
+
+X_reduced = TSNE(n_components=2, random_state=0).fit_transform(digits.data)
+
+print(X_reduced.shape)
+# (1797, 2)
+
+plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=digits.target)
+plt.colorbar()
+# plt.show() seabornでもmatplotlibでも表示されない．なぜか．
+# <matplotlib.colorbar.Colorbar at 0x7ff21173ee90>
